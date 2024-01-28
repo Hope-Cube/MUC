0c2dc93359be0a306d96c25ada6fe52b23d87d58
Hope-Cube <hopecube@gmail.com>
2024-01-28 12:39:54 +0100

    Add project files.

---
+++ MUC/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        WriteLine("Give the word per minute number.");
+        double wpm = double.Parse(ReadLine());
+        WriteLine("Your Ti's length is: " + (60.0 / (50 * wpm)));
+        ReadKey(true);
+    }
+}
